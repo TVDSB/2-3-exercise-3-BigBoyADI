f4528026bf8901feee2ed6899c0e616fbb4359c1
BigBoyADI <100423029+BigBoyADI@users.noreply.github.com>
2022-03-29 09:18:20 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,5 +1,14 @@
 def main():
-    #your code goes here
+    number = float(input("Enter a number "))
+
+    if number%15 == 0:
+       print("fizzbuzz")
+    elif number%5 == 0:
+       print("buzz")
+    elif number%3 == 0:
+       print("fizz")
+    else:
+       print("none of the cases are true")#your code goes here
     print("0")
 
 if __name__ =='__main__':
